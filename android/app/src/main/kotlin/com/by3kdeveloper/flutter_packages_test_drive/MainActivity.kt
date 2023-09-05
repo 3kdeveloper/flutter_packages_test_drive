636bcf0d222fc1335a6884c0b8cfb07678486ad5
3kdeveloper <kamrankhan@techswivel.com>
2023-09-05 15:07:01 +0500

    first commit

---
+++ android/app/src/main/kotlin/com/by3kdeveloper/flutter_packages_test_drive/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.by3kdeveloper.flutter_packages_test_drive
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ android/app/src/main/kotlin/com/by3kdveloper/flutter_packages_test_drive/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.by3kdveloper.flutter_packages_test_drive
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
